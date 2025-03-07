7e9708a08fa8fc972e3e164e6cd069fce32481af
pratikpr <pratikpr95@gmail.com>
2025-03-07 12:39:38 -0800

    49. Group Anagrams

--- 49-group-anagrams.py
+++ 49-group-anagrams.py
@@ -6,6 +6,7 @@ from typing import List
 def groupAnagrams(strs: List[str]) -> List[List[str]]:
     word_map = {}
     
+    # for more efficiency use a count array; reduces NKlogK to NK
     for word in strs:
        sorted_word = ''.join(sorted(word))
        word_map.setdefault(sorted_word, []).append(word)
