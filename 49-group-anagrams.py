7643b64b8cd24d952deb84b2a7207a74f7222fe5
pratikpr <pratikpr95@gmail.com>
2025-03-07 12:39:20 -0800

    49. Group Anagrams

---
+++ 49-group-anagrams.py
@@ -0,0 +1,20 @@
+# Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+from typing import List
+
+
+def groupAnagrams(strs: List[str]) -> List[List[str]]:
+    word_map = {}
+    
+    for word in strs:
+       sorted_word = ''.join(sorted(word))
+       word_map.setdefault(sorted_word, []).append(word)
+       
+    return [_list for _list in word_map.values()]
+
+strs = ["eat","tea","tan","ate","nat","bat"]
+print(groupAnagrams(strs))
+
+strs = ["a"]
+print(groupAnagrams(strs))
+
