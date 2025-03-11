53bcd953b8fd8ecf25158b989f20805544b01e75
pratikpr <pratikpr95@gmail.com>
2025-03-10 20:43:04 -0700

    15. 3Sum

--- neetcode-150/two-pointers/15-3sum.py
+++ neetcode-150/two-pointers/15-3sum.py
@@ -20,19 +20,20 @@ from typing import List
 def threeSum(nums: List[int]) -> List[List[int]]:
     nums.sort()
     res = []
+    goal = 0
     
     for i in range(len(nums)-2):
         if i > 0 and nums[i] == nums[i-1]:
             continue
         l,r = i+1, len(nums)-1
-        target = -nums[i]
+        target = goal-nums[i]
         while l < r:
             _sum = nums[l] + nums[r]
             if _sum == target:
                 res.append([nums[i], nums[l], nums[r]])
                 l += 1; r -= 1
-                # while l < r and nums[l] == nums[l-1]:
-                #         l += 1
+                while l < r and nums[l] == nums[l-1]:
+                        l += 1
             elif _sum > target:
                 r -= 1
             else:
